05b3be314bbe961b629f3674eee84b2f6f116171
Paolo <castagnettipaolo01@gmail.com>
2023-04-28 14:18:37 +0200

    Homework3 - Comments added for preprocessor

--- Homework_03/main.cpp
+++ Homework_03/main.cpp
@@ -1,3 +1,17 @@
+/*
+* The preprocessor is used in the first compilation stage, for each translation unit. 
+* It has a very limited command set:
+* #define: defines a symbol (which may have a value)
+* #undef: undefines a symbol
+* #ifdef / #ifndef / #elif / #else / #endif : preprocessor conditionals
+* #include: includes a file as a part of the current translation unit, by copying and pasting it.
+* 
+* 
+*/
+
+#define MACRO_VALUELESS
+#define MACRO 4
+
 int main(int argc, char** argv) {
 	return 0;
 }

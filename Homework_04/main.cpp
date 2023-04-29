8d5f6900be49fe5be120733ecac16d51f765cc0d
Paolo Castagnetti <castagnettipaolo01@gmail.com>
2023-04-29 09:27:08 +0200

    Homework4 - Function pointer (correct)

--- Homework_04/main.cpp
+++ Homework_04/main.cpp
@@ -1,12 +1,17 @@
 int add(int op1, int op2) {
 	return 0;
 }
+
 int main(int argc, char** argv) {
 	if (argc != 1) {
 		return 0;
 	}
 
-	int(*pfnAdd(int, int)) = &add;
+	//How to create a function pointer
+	{
+		int (*pfnAdd)(int, int) = &add;
+		int result = pfnAdd(2, 3);
+	}
 	
 	return 0;
 }

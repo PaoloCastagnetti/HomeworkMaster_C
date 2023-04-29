0cbfab3e9c15f0733123ea2a603a41167cfd8d89
Paolo Castagnetti <castagnettipaolo01@gmail.com>
2023-04-29 10:46:34 +0200

    Homework4 - Dynamic memory allocation update

--- Homework_04/DynamicMemoryExamples.cpp
+++ Homework_04/DynamicMemoryExamples.cpp
@@ -2,15 +2,27 @@
 int main() {
 
 	//measure unit: byte 
+	
 	//C-style dynamic allocation
 	//void* malloc(size_t sz);
 	int* ptr = (int*)malloc(sizeof(int));
 	//void* calloc(size_t sz);
-	int* zptr = (int*)calloc(sizeof(int));
+	int* zptr = (int*)calloc(1, sizeof(int));
 	//void free(void* ptr);
 	free(ptr);
 	free(zptr);
 
+	//C++ style dynamic allocation
+	//void* operator new(size_t sz); allocates sz bytes
+	int* ptr = new int;
+	int* fptr = new int(5);
+	//void* operator new[](size_t sz); allocates sz bytes
+	int* aptr = new int[5];
+	//void operator delete(void* ptr); frees a memory block
+	delete ptr;
+	delete fptr;
+	//void operator delete[](void* ptr); frees a memory block
+	delete[] aptr;
 
 	return 0;
 }

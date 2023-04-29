c164d181a9bf62220cdb4cd8a5af18a557626ac1
Paolo Castagnetti <castagnettipaolo01@gmail.com>
2023-04-29 10:07:14 +0200

    Homework4 - Small changes

--- Homework_04/ArrayExamples.cpp
+++ Homework_04/ArrayExamples.cpp
@@ -4,7 +4,7 @@ int main() {
 	int myArray[3]; //only declaration
 	int myData[3] = { 1,2,3 }; //declaration and initialization
 	int myDataArray[] = { 1,2,3 }; //declaration and initialization
-	int test[100] = { 0 }; //all the other 99 elements are sets to zero
+	int test[100] = {}; //all the elements are sets to zero
 
 	return 0;
 }

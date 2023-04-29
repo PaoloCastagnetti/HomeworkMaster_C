80af75386d1cf13cafc4ab044af0425181e34df2
Paolo Castagnetti <castagnettipaolo01@gmail.com>
2023-04-29 09:47:55 +0200

    Homework4 - Example function pointer for a button

---
+++ Homework_04/Examplebutton.cpp
@@ -0,0 +1,26 @@
+/*
+* This file is an example of a function pointer used into a button
+*/
+int GenericFunction(int a, int b);
+
+int OnClickDetected() {
+	int a = 42;
+	int b = 43;
+	return GenericFunction(a, b);
+}
+
+int main() {
+	//button->AddDunctionToButton(&OnClickDetected);
+	return 0;
+}
+
+//...Inside the button...
+void(*OnClick)() = 0L;
+
+void eventClick() {
+	OnClick();
+}
+
+void AddFunctionToButton(void (*funptr)()) {
+	OnClick = funptr;
+}

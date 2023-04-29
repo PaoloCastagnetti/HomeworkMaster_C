3a54dac7468937860d0a6621ebc61f5b40545e84
Paolo Castagnetti <castagnettipaolo01@gmail.com>
2023-04-29 09:59:52 +0200

    Homework4 - Array example file added

---
+++ Homework_04/ArrayExamples.cpp
@@ -0,0 +1,10 @@
+int main() {
+
+	//4 type of declaration in c++ for arrays
+	int myArray[3]; //only declaration
+	int myData[3] = { 1,2,3 }; //declaration and initialization
+	int myDataArray[] = { 1,2,3 }; //declaration and initialization
+	int test[100] = { 0 }; //all the other 99 elements are sets to zero
+
+	return 0;
+}

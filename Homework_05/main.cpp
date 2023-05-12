00a08fda594d19aba9bd2deb920a7a6e33c0f22e
Paolo Castagnetti <castagnettipaolo01@gmail.com>
2023-05-12 15:10:43 +0200

    Homework5 - Some usages

--- Homework_05/main.cpp
+++ Homework_05/main.cpp
@@ -1,7 +1,42 @@
+#include <iostream>
+//Enum definition
+enum Colors {
+	White = 0,
+	Red,
+	Black,
+	Color_Count
+};
+
+//Struct definition
+struct myStruct {
+	int intValue;
+	char charValue;
+};
+
+//Union definition
+union Color {
+	unsigned int ARGB;
+	char bytes[4];
+};
+
 int main(int argc, char** argv) {
 	if (argc != 1) {
-		return;
+		return 0;
 	}
 
+	//Enum usage
+	Colors colors;
+	colors = Colors::White;
+
+	//Struct usage
+	myStruct sstruct;
+	sstruct.intValue = 0;
+	sstruct.charValue = 's';
+
+	//Union usage
+	Color myColor;
+	myColor.ARGB = 64;
+	//std::cout << "Color 64: " << myColor.bytes << std::endl;
+	
 	return 0;
 }

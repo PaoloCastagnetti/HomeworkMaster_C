c8dd48efe1ae7c7b54d667ee582fd32118b4deba
Paolo Castagnetti <castagnettipaolo01@gmail.com>
2023-05-13 10:10:42 +0200

    Homework5 - Documentation added

--- Homework_05/testClass.cpp
+++ Homework_05/testClass.cpp
@@ -1,18 +1,16 @@
 #include "testClass.h"
+//Class functions definition
 MyClass::MyClass() {
 	privateVariable = 0;
 	publicVariable = 0;
 }
-
 MyClass::MyClass(int value) {
 	privateVariable = value;
 	publicVariable = value;
 }
-
 int MyClass::getPrivateVariable() {
 	return  privateVariable;
 }
-
 void MyClass::setPrivateVariable(int value) {
 	privateVariable = value;
 }
--- Homework_05/testClass.h
+++ Homework_05/testClass.h
@@ -6,8 +6,25 @@ private:
 
 public:
 	int publicVariable;
+	/*
+	* @brief getPrivateVariable return the value of the privateVariable
+	* @return the value of the privateVariable
+	*/
 	int getPrivateVariable();
+	/*
+	* @brief setPrivateVariable set the value of the privateVariable
+	* @param the value to assign tp the privateVariable
+	*/
 	void setPrivateVariable(int value);
+	/*
+	* @brief MyClass constructor
+	* @return an instance of the class
+	*/
 	MyClass();
+	/*
+	* @brief MyClass constructor
+	* @param value to set all the variables in the class
+	* @return an instance of the class
+	*/
 	MyClass(int value);
 };
